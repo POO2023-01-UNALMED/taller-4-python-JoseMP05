9c6b43507ecafb4f883f856bbfe8f9a7b241156a
Jose Miguel Pulgarin A <jjosemii64@gmail.com>
2023-03-06 21:40:22 -0500

    fixing error

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -4,20 +4,18 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
     def listadoAsignaturas(self, **kwargs):
-        if self._asignaturas is None:
-            self._asignaturas = []
+        self._asignaturas = []
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=None):
-        if self.listadoAlumnos is None:
-            self.listadoAlumnos = []
+        self.listadoAlumnos = []
         if lista is not None:
             lista.append(alumno)
             for item in lista:
