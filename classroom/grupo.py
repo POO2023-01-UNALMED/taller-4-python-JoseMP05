b82ee6247f4ec98607142ab679b80daae5254e3c
Jose Miguel Pulgarin A <jjosemii64@gmail.com>
2023-03-06 21:30:44 -0500

    correcting code

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,42 @@
 from classroom.asignatura import Asignatura
 
-class Grupo:
-    grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+class Grupo:
+    grado = "Grado 12"
+
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
+        if self._asignaturas is None:
+            self._asignaturas = []
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, lista=None):
+        if self.listadoAlumnos is None:
+            self.listadoAlumnos = []
+        if lista is not None:
+            lista.append(alumno)
+            for item in lista:
+                self.listadoAlumnos.append(item)
+        else:
+            self.listadoAlumnos.append(alumno)
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        cadena = "Grupo de estudiantes" + ': ' + self._grupo
+        return cadena
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    @ staticmethod
+    def asignarNombre(cls, nombre="Grado 12"):
         cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 4"):
+        cls.grado = nombre
+
+    @ classmethod
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
